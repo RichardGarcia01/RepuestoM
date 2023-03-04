8109c884269f70b6968006ebb56fbd0e6a9895a2
RichardGarcia01 <rg0329221@gmail.com>
2023-03-03 20:30:19 -0400

    2-iniciacion

--- Server/models/UsuarioRol.cs
+++ Server/models/UsuarioRol.cs
@@ -19,7 +19,26 @@ public class UsuarioRol
  public bool PermisoParaEliminar{get;set;}
  public virtual ICollection<UsuarioRol>? Usuarios {get;set;}= null!;
 
-   public UsuarioRollRecords ToRecords()
+public static UsuarioRol Create(UsuarioRolCreateRequest request)
+{
+    return new UsuarioRol(){
+        Nombre= request.Nombre,
+        PermisoParaCrear= request.PermisoParaCrear,
+        PermisoParaEditar= request.PermisoParaEditar,
+        PermisoParaEliminar= request.PermisoParaEliminar,
+    };
+}
+public void Modificar(UsuarioRolUpdateRequest request){
+    if(Nombre!= request.Nombre)
+       Nombre = request.Nombre;
+    if(PermisoParaCrear!= request.PermisoParaCrear)
+       PermisoParaCrear = request.PermisoParaCrear;
+    if(PermisoParaEditar != request.PermisoParaEditar)
+       PermisoParaEditar = request.PermisoParaEditar;
+    if(PermisoParaEliminar != request.PermisoParaEliminar)
+       PermisoParaEditar = request.PermisoParaEliminar;      
+}
+public UsuarioRollRecords ToRecords()
 {
     return new UsuarioRollRecords(Id,Nombre,PermisoParaCrear,PermisoParaEditar,PermisoParaEliminar);
 }
